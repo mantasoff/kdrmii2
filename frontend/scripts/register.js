26d9dd4c809ce33e901358a185097a5d4cf29eb3
Molester <jjmindaugas@gmail.com>
2018-04-25 21:06:57 +0300

    Form update 2

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -1,6 +1,12 @@
 function notEmpty(name, value) {
     var x = value;
-	if (x.length > 0)	{
-		return document.getElementById(name+"_ID").innerHTML = "You wrote: " + x;
+	if (x.length === 0)	{
+		return document.getElementById(name+"_ID").innerHTML = "Please fill out this field";
 	}
+}
+function isLastChoice(name, value) {
+
+}
+function defaultValues() {
+	
 }
